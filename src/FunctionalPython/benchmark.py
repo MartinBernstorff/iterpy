6ced6dbc7686dd1a26afd684f3c81c35ebd1aad5
Martin Bernstorff <martinbernstorff@gmail.com>
2023-09-29 13:47:23 +0200

    refactor: split out calculations into functions

--- src/FunctionalPython/benchmark.py
+++ src/FunctionalPython/benchmark.py
@@ -14,7 +14,7 @@ class LineStatus(Enum):
 
 
 @dataclass(frozen=True)
-class ItemResults:
+class Item:
     ship_date: dt.datetime
     quantity: int
     extended_price: float
@@ -25,7 +25,7 @@ class ItemResults:
 
 
 @dataclass(frozen=True)
-class CategoryResults:
+class CategorySummary:
     group_name: str
     sum_quantity: int
     sum_base_price: float
@@ -38,26 +38,34 @@ class CategoryResults:
     num_orders: int
 
 
-def get_aggregate_data(input_data: Group[ItemResults]) -> CategoryResults:
+def summarise_category(input_data: Group[Item]) -> CategorySummary:
     group_id = input_data.group_id
-    rows = input_data.group_contents
+    rows = input_data.group_contents.to_list()
 
-    return CategoryResults(
+    return CategorySummary(
         group_name=group_id,
-        sum_quantity=sum(row.quantity for row in rows),
-        sum_base_price=sum(row.extended_price for row in rows),
-        sum_discount_price=sum(row.extended_price * (1 - row.discount) for row in rows),
-        sum_charge=sum(row.extended_price * row.discount * row.tax for row in rows),
-        avg_quantity=stats.mean(row.quantity for row in rows),
-        avg_price=stats.mean(row.extended_price for row in rows),
-        avg_discount=stats.mean(row.discount for row in rows),
-        num_orders=len(rows),
+        sum_quantity=sum(r.quantity for r in rows),
+        sum_base_price=sum(r.extended_price for r in rows),
+        sum_discount_price=sum(calculate_discounted_price(r) for r in rows),
+        sum_charge=sum(calculate_charge(r) for r in rows),
+        avg_quantity=stats.mean(r.quantity for r in rows),
+        avg_price=stats.mean(r.extended_price for r in rows),
+        avg_discount=stats.mean(r.discount for r in rows),
+        num_orders=input_data.group_contents.count(),
     )
+
+
+def calculate_discounted_price(item: Item) -> float:
+    return item.extended_price * (1 - item.discount)
+
+
+def calculate_charge(r: Item) -> float:
+    return r.extended_price * r.discount * r.tax
 
 
 if __name__ == "__main__":
     input_data = [
-        ItemResults(
+        Item(
             ship_date=dt.datetime(1995 + i, 9, 2),
             quantity=i,
             extended_price=0.1 * i,
@@ -69,6 +77,11 @@ if __name__ == "__main__":
         for i in range(10)
     ]
 
-    sequence = Seq(input_data).group_by(lambda row: row.line_status.name)
+    sequence = (
+        Seq(input_data)
+        .group_by(lambda row: f"status_{row.line_status.name}_returned_{row.returned}")
+        .map(summarise_category)
+        .to_list()
+    )
 
     pass
--- src/FunctionalPython/sequence.py
+++ src/FunctionalPython/sequence.py
@@ -11,18 +11,21 @@ _T1 = TypeVar("_T1")
 @dataclass(frozen=True)
 class Group(Generic[_T0]):
     group_id: str
-    group_contents: Sequence[_T0]
+    group_contents: "Seq[_T0]"
 
 
 class Seq(Generic[_T0]):
     def __init__(self, iterable: Iterable[_T0]):
         self._seq = iterable
 
+    def count(self) -> int:
+        return sum(1 for _ in self._seq)
+
     def to_list(self) -> list[_T0]:
         return list(self._seq)
 
-    def to_generator(self) -> Iterator[_T0]:
-        return (i for i in self._seq)
+    def to_iter(self) -> Iterator[_T0]:
+        return iter(self._seq)
 
     def map(  # noqa: A003 # Ignore that it's shadowing a python built-in
         self,
@@ -38,7 +41,7 @@ class Seq(Generic[_T0]):
 
     def group_by(self, func: Callable[[_T0], str]) -> "Seq[Group[_T0]]":
         result = (
-            Group(group_id=key, group_contents=list(value))
+            Group(group_id=key, group_contents=Seq(value))
             for key, value in itertools.groupby(self._seq, key=func)
         )
         return Seq(result)
