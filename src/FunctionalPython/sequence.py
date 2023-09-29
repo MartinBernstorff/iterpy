f21b194164f03cb782e63081a2fc5e0c5f38ac08
Martin Bernstorff <martinbernstorff@gmail.com>
2023-09-29 11:33:35 +0200

    misc.

--- src/FunctionalPython/sequence.py
+++ src/FunctionalPython/sequence.py
@@ -1,4 +1,5 @@
-from typing import Callable, Generic, Iterable, Sequence, TypeVar
+from functools import reduce
+from typing import Callable, Generic, Iterable, Iterator, Sequence, TypeVar
 
 _ContainedType = TypeVar("_ContainedType")
 _T1 = TypeVar("_T1")
@@ -11,8 +12,28 @@ class Seq(Generic[_ContainedType]):
     def to_list(self) -> list[_ContainedType]:
         return list(self._seq)
 
-    def to_iter(self) -> Iterable[_ContainedType]:
-        return self._seq
+    def to_generator(self) -> Iterator[_ContainedType]:
+        return (i for i in self._seq)
 
     def map(self, func: Callable[[_ContainedType], _T1]) -> "Seq[_T1]":  # noqa: A003
         return Seq(map(func, self._seq))
+
+    def filter(  # noqa: A003
+        self, func: Callable[[_ContainedType], bool]
+    ) -> "Seq[_ContainedType]":
+        return Seq(filter(func, self._seq))
+
+    def reduce(
+        self, func: Callable[[_ContainedType, _ContainedType], _ContainedType]
+    ) -> _ContainedType:
+        return reduce(func, self._seq)
+
+    def flatten(self) -> "Seq[_ContainedType]":
+        return Seq(
+            (
+                item
+                for sublist in self._seq
+                for item in sublist
+                if isinstance(sublist, Sequence)
+            )
+        )
--- src/FunctionalPython/test_sequence.py
+++ src/FunctionalPython/test_sequence.py
@@ -1,7 +1,31 @@
 from FunctionalPython.sequence import Seq
 
 
+def test_chaining():
+    sequence = Seq([1, 2])
+    result = sequence.filter(lambda x: x % 2 == 0).map(lambda x: x * 2).to_list()
+    assert result == [4]
+
+
 def test_map():
     sequence = Seq([1, 2])
     result = sequence.map(lambda x: x * 2).to_list()
     assert result == [2, 4]
+
+
+def test_filter():
+    sequence = Seq((1, 2))
+    result = sequence.filter(lambda x: x % 2 == 0).to_list()
+    assert result == [2]
+
+
+def test_reduce():
+    sequence = Seq([1, 2])
+    result = sequence.reduce(lambda x, y: x + y)
+    assert result == 3
+
+
+def test_flatten():
+    sequence = Seq([[1, 2], [3, 4]])
+    result = sequence.flatten().to_list()
+    assert result == [1, 2, 3, 4]
