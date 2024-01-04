5d4f08e7ccb003c52176498d9fdb823f7281e9dd
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-04 11:23:01 +0000

    fix: rename

--- iterpy/benchmark/query_1/polars_q1.py
+++ iterpy/benchmark/query_1/polars_q1.py
@@ -2,8 +2,8 @@ import datetime as dt
 
 import polars as pl
 
-from iter.benchmark.query_1.input_data import Q1_DATA
-from iter.benchmark.utils import benchmark_method
+from iterpy.benchmark.query_1.input_data import Q1_DATA
+from iterpy.benchmark.utils import benchmark_method
 
 
 def main(data: pl.LazyFrame) -> pl.DataFrame:
